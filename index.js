5dd1bc7e1ffb586dc1fb5b051d28bc2bef494cc1
Mitchell Snow <snow801mitch@gmail.com>
2018-12-21 09:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function myAnimal() {
   return animal
 }
 
-/* function yourAnimal() {
+ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
-} */
+  return myAnimal()
+}
 
 function add2(n) {
   return n + two
